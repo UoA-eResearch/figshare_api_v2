d69dfe22fac1a65edc41c07e651219f65a063c9b
robertburrowes <r.burrowes@auckland.ac.nz>
2020-11-05 11:08:53 +1300

    Upload test of directory

--- test/upload.rb
+++ test/upload.rb
@@ -3,10 +3,16 @@ require_relative '../lib/figshare_api_v2.rb'
 @figshare = Figshare::Init.new(figshare_user: 'uploadTest', conf_dir: "#{__dir__}/conf")
 
 TEST_ARTICLE_ID=13087316
-TEST_FILE="#{__dir__}/test_data/native_bee.mov"
+TEST_DIR="#{__dir__}/test_data"
 
-@figshare.upload.upload(article_id: TEST_ARTICLE_ID, file_name: TEST_FILE, trace: true)
+puts "List test articles files"
+@figshare.private_articles.files(article_id: TEST_ARTICLE_ID) { |f| p f }
+
+puts
+puts "Upload Dir #{TEST_DIR}"
+@figshare.upload.upload_dir(article_id: TEST_ARTICLE_ID, directory: TEST_DIR, trace: 2)
+
 puts
 puts "List test articles files"
-@figshare.public_articles.files(article_id: TEST_ARTICLE_ID)
+@figshare.private_articles.files(article_id: TEST_ARTICLE_ID) { |f| p f }
 
