2f8612c57e2ae314a9ec320a8d04ab7bb71e5f33
robertburrowes <r.burrowes@auckland.ac.nz>
2021-03-19 13:24:00 +1300

    changes from hash to json naming

--- test/article_detail.rb
+++ test/article_detail.rb
@@ -4,13 +4,13 @@ require_relative '../lib/figshare_api_v2.rb'
 ARGV = ['conf/upload2.json']
 #First and only argument is the configuration file.
 article_conf = JSON.parse(File.read(ARGV[0]))
+
 @figshare = Figshare::Init.new(figshare_user: article_conf['figshare_user'], conf_dir: article_conf['figshare_conf_dir'])
 
 article_list = article_conf['article_list']
 
 article_list.each do |a|
-  @figshare.private_articles.detail(article_id: a[:article_id], impersonate: article_conf['imperonate']) do |r|
-    p "#{r['title']}, #{r['doi']}"
-    puts
+  @figshare.private_articles.detail(article_id: a['article_id'], impersonate: article_conf['impersonate']) do |r|
+    p "title: #{r['title']}, doi: #{r['doi']}"
   end
 end
