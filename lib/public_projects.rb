43398fbffacbfb386233bc19b2d442070d37050a
robertburrowes <r.burrowes@auckland.ac.nz>
2022-05-10 10:27:57 +1200

    API changes

--- lib/public_projects.rb
+++ lib/public_projects.rb
@@ -9,14 +9,32 @@ module Figshare
     # @param published_since [Time] Return results if published after this time
     # @param order [String] "published_date" Default, "modified_date", "views", "cites", "shares"
     # @param order_direction [String] "desc" Default, "asc"
+    # @param page [Numeric] Pages start at 1. Page and Page size go together
+    # @param page_size [Numeric]
+    # @param offset [Numeric] offset is 0 based.  Offset and Limit go together
+    # @param limit [Numeric]
     # @yield [Hash] {url, published_date, id, title}
-    def list(institute: false, group_id: nil, published_since: nil, order: 'published_date', order_direction: 'desc', &block)
+    def list( institute: false,
+              group_id: nil,
+              published_since: nil,
+              order: 'published_date',
+              order_direction: 'desc',
+              page: nil,
+              page_size: nil,
+              offset: nil,
+              limit: nil,
+              &block
+            )
       args = {}
       args['institution'] = @institute_id unless institute.nil?
       args['group'] = group_id unless group_id.nil?
       args['published_since'] = published_since unless published_since.nil?
       args['order'] = order unless order.nil?
       args['order_direction'] = order_direction unless order_direction.nil?
+      args['page'] = page unless page.nil?
+      args['page_size'] = page_size unless page_size.nil?
+      args['offset'] = offset unless offset.nil?
+      args['limit'] = limit unless limit.nil?
       get_paginate(api_query: 'projects', args: args, &block)
     end
 
